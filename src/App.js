b202090c458e2c5729b26537bb14d4ba5139bb57
IrshadBagwan <irshadbagwan706@gmail.com>
2023-08-10 11:09:57 +0530

    crypto-hunter

--- src/App.js
+++ src/App.js
@@ -1,59 +1,34 @@
-import { Fragment, useState } from 'react';
-import Header from './Components/Layout/Header';
-import Items from './Components/Meals/Items'
-import Cart from './Components/Cart/Cart';
-import CartProvider from './Store/CartProvider';
-import CartContext from './Store/CartContext';
-import Practise from './Components/MealItem/practise'
-import { BrowserRouter as Router,Route,Link,Switch,Routes, BrowserRouter } from 'react-router-dom';
-import Aboutus from './Components/Meals/Aboutus';
-import Home from './Components/Meals/Home';
-import Contactus from './Components/Layout/Contactus';
-import Singleproductpage from './Components/Singleproductpage/Singleproductpage';
+
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Header from './Components/Header.js';
+import HomePage from "./pages/HomePage.js";
+import CoinPage from "./pages/CoinPage.js";
+import {makeStyles} from "@material-ui/core";
 
 
-function App() {
+const App = () =>{
 
- 
+  const useStyles = makeStyles(() => ({
+    App:{
+     backgroundColor: "#14161a",
+     color:"white",
+     minHeight:"100vh"
+    }
+  }));
 
-  const [cartIsShown, setCartIsShown] = useState(false);
-
-  const showCartHandler = () =>{
-    setCartIsShown(true);
-  }
-
-  const hideCartHandler = () =>{
-    setCartIsShown(false);
-  }
-
-  return (
-    <CartProvider>
-    
-      
-      
-      {cartIsShown && <Cart onClose={hideCartHandler}/>}
-      <Header onShowCart={showCartHandler} />
-    
-      <BrowserRouter>
-     <Routes>
-       
-       
-    
-        <Route path='/' element={<Items/>}/>
-        <Route path='store' element={<Items/>}/>
-        <Route path="/home" element={<Home/>}/>
-       <Route path="aboutus" element={<Aboutus/>}/>
-       <Route path="Contactus" element={<Contactus/>}/>
-       <Route path="/store/product/:id" element={ <Singleproductpage/>}>
-        
-       </Route>
-      
-       </Routes>
-      </BrowserRouter>
-      
-      
-    </CartProvider>
+  const classes = useStyles();
+  return(
+    <BrowserRouter>
+    <div className={classes.App}>
+      <Header/>
+      <Routes>
+      <Route path="/" element={<HomePage/>}  exact/>
+      <Route path="/coins/:id" element={<CoinPage/>} />
+      </Routes>
+    </div>
+    </BrowserRouter>
   );
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import App from './App';
 // React Bootstarp Configuration
 import '../node_modules/react-bootstrap/dist/react-bootstrap';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import CryptoContext from './CryptoContext';
+import  "react-alice-carousel/lib/alice-carousel.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<CryptoContext><App /></CryptoContext>);
